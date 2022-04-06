be534dade6ca907c6df0f06c0563d83d1dfe1828
junior panichphol <newandovergame@gmail.com>
2022-04-07 01:14:13 +0700

    regis & login auth
    #regis & login (JWT)
    #connect mongodb

--- server.js
+++ server.js
@@ -6,36 +6,45 @@ let express = require('express'),
     cors = require('cors'),
     bodyParser = require('body-parser'),
     dotenv = require('dotenv').config()
-    dbConfig = require('./database/db');
+    dbConfig = require('./config/db');
     path = require('path');
 
 const app = express()
-
-
-
-
-
 
 // // เรียกใช้ app
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(cors());
 
 
 // app.use('/post', postRoute);
-// app.use('/user', userRoutes);
-
-// // ใช้ deploy project
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../build')))           
-
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.join(__dirname, "../build/index.html"))   
-//     })
-// }
+app.use('/users', require('./routes/userRoutes'))
 
 
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.db, {useNewUrlParser: true,  useUnifiedTopology: true
+    })
+    .then(() => {
+    console.log('Database successfully connected');
+}, 
+    error => {
+        console.log('Could not connect to database: ' + error)
+    }
+)
+
+
+
+// ใช้ deploy project
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../build')))           
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../build/index.html"))   
+    })
+}
 
 
 // PORT
@@ -45,3 +54,14 @@ app.listen(port, () => console.log(`Server started on port ${port}`))
 //     console.log('Connected to port ' + port)
 // })
 
+// 404 Error
+app.use((req, res, next) => {
+    next(createError(404))
+})
+
+// Error handler
+app.use(function(err, req, res, next) {
+    console.error(err.message);
+    if (!err.statusCode) err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+})
